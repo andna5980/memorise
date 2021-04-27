95f37ab31363a6abec29e867b92925e5c9e1b547
Andres Navarrete <andy5980@hotmail.com>
2021-04-27 18:16:35 +0100

    Styling Game box WIP

---
+++ .history/assets/js/main_20210427181602.js
@@ -0,0 +1,18 @@
+//Colors array
+let colors = ['black','blue','yellow', 'white','brown'];
+
+//get button
+let button = document.getElementById('changecolor');
+
+//add Event
+button.addEventListener('click', function(){
+    
+    //add random color
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]
+    let container = document.getElementById('game-box')
+
+    container.style.background = randomColor;
+})
+
+
+
