a60e45d4ebe2d9a6af744d4f462a70bb034033ad
Andres Navarrete <andy5980@hotmail.com>
2021-04-27 16:39:39 +0100

    adding style to game box

---
+++ .history/assets/js/main_20210427163055.js
@@ -0,0 +1,9 @@
+let colors = ['black','blue','yellow', 'white','brown'];
+
+//let button = document.querySelector("#button");
+let button = document.getElementsByClassName('.changecolor');
+
+changecolor.addEventListener('click', function(){
+    console.log('test')
+})
+
